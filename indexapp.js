7b9d30a2bb1176d740dfa8e4ca2c610072844db2
cold <coldmagmaig@gmail.com>
2020-03-31 09:07:53 +0530

    optimized javascript

--- indexapp.js
+++ indexapp.js
@@ -1,15 +1,16 @@
 function bgChange() {
-	if(this.scrollY > 1 * this.innerHeight / 3 && this.scrollY < 2 * this.innerHeight / 3){
-		console.log('success');
+	if(this.scrollY > this.innerHeight / 3 && this.scrollY < 2 * this.innerHeight/3 + 500){
+		console.log(this.innerHeight + "1");
 		document.body.classList.add("activebg");
 		document.body.classList.remove("activebg2");
 	}
-	else if(this.scrollY > 2 * this.innerHeight / 3){
-		console.log('no');
+	else if(this.scrollY > 2 * this.innerHeight/3 + 500){
+		console.log(this.innerHeight + "2");
 		document.body.classList.add("activebg2");
 		document.body.classList.remove("activebg");
 	}
 	else{
+		console.log(this.innerHeight + "3");
 		document.body.classList.remove("activebg");
 		document.body.classList.remove("activebg2");
 	}
