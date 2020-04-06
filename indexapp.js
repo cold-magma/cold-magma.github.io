5dfc3dd8ba53efddd3afd9db3337c2e90c4100aa
cold <coldmagmaig@gmail.com>
2020-04-06 15:28:32 +0530

    activebg3 added in css for index

--- indexapp.js
+++ indexapp.js
@@ -1,16 +1,24 @@
 function bgChange() {
-	if(this.scrollY > this.innerHeight / 3 && this.scrollY < 2 * this.innerHeight/3 + 500){
+	if(this.scrollY > this.innerHeight / 4 && this.scrollY < this.innerHeight/ 2 + 500){
 		console.log(this.innerHeight + "1");
 		document.body.classList.add("activebg");
 		document.body.classList.remove("activebg2");
+		document.body.classList.remove("activebg3");
 	}
-	else if(this.scrollY > 2 * this.innerHeight/3 + 500){
+	else if(this.scrollY > this.innerHeight/2 + 500 && this.scrollY < 3 * this.innerHeight/ 4 + 1000){
 		console.log(this.innerHeight + "2");
 		document.body.classList.add("activebg2");
 		document.body.classList.remove("activebg");
+		document.body.classList.remove("activebg3");
+	}
+	else if(this.scrollY > 3 * this.innerHeight/ 4 + 1000){
+		console.log(this.innerHeight + "3");
+		document.body.classList.add("activebg3");
+		document.body.classList.remove("activebg");
+		document.body.classList.remove("activebg2");
 	}
 	else{
-		console.log(this.innerHeight + "3");
+		document.body.classList.remove("activebg3");
 		document.body.classList.remove("activebg");
 		document.body.classList.remove("activebg2");
 	}
