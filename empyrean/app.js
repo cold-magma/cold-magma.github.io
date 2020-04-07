c10bb52ecccb7877f297412eea53fb5e73a1a4c0
cold <coldmagmaig@gmail.com>
2020-04-07 14:12:17 +0530

    more code optimizaion

--- empyrean/app.js
+++ empyrean/app.js
@@ -1,26 +1,9 @@
 window.onload = function() {
 	$(".loader-wrapper").fadeOut("slow");
-
 	lax.setup() // init
-
 	const updateLax = () => {
 		lax.update(window.scrollY)
 		window.requestAnimationFrame(updateLax)
 	}
-
 	window.requestAnimationFrame(updateLax)
-	updatescreen()
-}
-
-updatescreen();
-
-function updatescreen() {
-	if(window.innerWidth < 600){
-		document.getElementById("d1").classList.add("small");
-		document.getElementById("d2").classList.add("small");
-	}else{
-		
-		document.getElementById("d1").classList.remove("small");
-		document.getElementById("d2").classList.remove("small");
-	}
 }
