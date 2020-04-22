16b88364c5f853d76631025350e6510d0540a831
cold <coldmagmaig@gmail.com>
2020-04-22 15:17:56 +0530

    mirage

--- mirage/app.js
+++ mirage/app.js
@@ -1,2 +1,11 @@
 var scene = document.getElementById('scene');
 var parallaxInstance = new Parallax(scene);
+
+document.body.addEventListener("click", popup);
+
+function popup(){
+	var dialog = document.getElementById("popupdiv");
+	var dialogbg = document.getElementById("popupdivbg");
+	dialogbg.classList.toggle("show");
+	dialog.classList.toggle("show");
+}
